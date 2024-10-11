41628ef2a1d073ad6ec11a7120a719b1090ef6d2
Ampomaah24 <165961822+Ampomaah24@users.noreply.github.com>
2024-10-11 17:54:31 +0000

    Updated to three services

--- lab5_files2/flask_app/app.py
+++ lab5_files2/flask_app/app.py
@@ -1,16 +1,26 @@
 from flask import Flask
 import redis
+import psycopg2
 
 app = Flask(__name__)
 
 r = redis.Redis(host="redis", port=6379)
 
+conn = psycopg2.connect(
+    host="db",
+    database="database",
+    user="user",
+    password="password"
+)
+
+cur = conn.cursor()
 
 @app.route("/")
 def home():
     count = r.incr("hits")
+    cur.execute("INSERT INTO hits (count) VALUES (%s)", (count,))
+    conn.commit()
     return f"This page has been visited {count} times."
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
